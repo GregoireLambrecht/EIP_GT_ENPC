61a8d7a08653c3634c7948b2a0a7cfef524a7fa8
micagent007 <99116765+micagent007@users.noreply.github.com>
2023-05-17 17:40:21 +0200

    Add files via upload

---
+++ region_growing.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+#ATTENTION ////////////////////////////////////////////////////////////////////
+#YOU HAVE TO RUN image_processing.py before runing this file
+#////////////////////////////////////////////////////////////////////////////// 
+
+
+#PERFORM ONE REGION GROWING 
+#img : 2D array containing only 0 and 255. 
+#seed_point : origin of the region growing
+#show = True : show the result
+def find_surface(img,seed_point,fill_color = 100, show = False):
+    # Set the threshold for region growing
+    threshold = 1
+    # Set the connectivity (4 or 8) for region growing
+    connectivity = 4
+    # Initialize the mask for region growing
+    mask = np.zeros((img.shape[0]+2, img.shape[1]+2), np.uint8)
+    # Perform region growing only on white regions
+    if img[seed_point[1], seed_point[0]] == 255:
+        cv2.floodFill(img, mask, seed_point, fill_color, threshold, threshold, connectivity)
+    if show :
+        cv2.imshow('Result', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return img
+
+
+#PERFORM REGION GROWING 
+#img : 2D array containing only 0 and 255
+def segmentation(img, show = True, process = True, er_kern = 7, di_kern = 5): 
+    if process:
+        img = erode(img,kern = er_kern,  iteration=1)                                    # add erosion step
+        img = dilation(img, kern = di_kern, iteration = 1)                               # add dilation step
+    # Perform region growing
+    connectivity = 4                                                       #4 or 8 : impact the definition of two nearby pixel                     
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img, connectivity, cv2.CV_32S)
+    fill_color = 250
+    #COLOR THE IMAGE 
+    for X in centroids: 
+        img = find_surface(img, (int(X[0]),int(X[1])),fill_color)
+        fill_color -= 5  
+    # Display the result
+    if show:
+        cv2.imshow('Result', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return img,labels, stats ,centroids
+        
+###############################################################################
+###############################################################################              
+# Load image as grayscale
+img = cv2.imread("C:/Users/grego/github/EIT_GT_ENPC/6.jpeg", cv2.IMREAD_GRAYSCALE) 
+#put a filter
+
+img = threshold(img,110)
+
+plt.imshow(img)
+
+#segmentation
+n_img,labels,stats,  centroids = segmentation(img)
+
+print(len(centroids))
+
+
+
+
+        
+                
+
+            
+            
